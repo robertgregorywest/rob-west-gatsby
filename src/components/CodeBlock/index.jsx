a8a20f9b9a4a9ce63f255bdced049ee35154dab3
Rob West <robertgregorywest@gmail.com>
2020-06-17 14:21:57 +0100

    Changed code block to highlight on SSR

--- src/components/CodeBlock/index.jsx
+++ src/components/CodeBlock/index.jsx
@@ -1,39 +1,24 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import hljs from './custom-hljs';
 import './style.scss';
 
 class CodeBlock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.codeNode = React.createRef();
-  }
-
-  componentDidMount() {
-    this.updateCodeSyntaxHighlighting();
-  }
-
-  componentDidUpdate() {
-    this.updateCodeSyntaxHighlighting();
-  }
-
-  updateCodeSyntaxHighlighting = () => {
-    hljs.highlightBlock(this.codeNode.current);
-  };
-
   render() {
     const {
       linkedItem: {
         elements: {
-          class: { value: className },
+          class: { value: language },
           code: { value: code },
         },
       },
     } = this.props;
+
+    const { value: formattedCode } = hljs.highlight(language, code);
+
     return (
       <pre>
-        <code ref={this.codeNode} className={className}>
-          {code}
-        </code>
+        <code className={`hljs ${language}`} dangerouslySetInnerHTML={{ __html: formattedCode }} />
       </pre>
     );
   }
