b02c1fd402ed6cf1b716383e6d41e45c154a26de
Rob West <robertgregorywest@gmail.com>
2025-10-04 16:02:43 +0100

    Adding in new head component

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
-export default function Head({ title, description }) {
+export default function Head({ title, description, canonical }) {
   const defaultTitle = "Rob West's Website";
   return (
     <>
       <title>{title ? `${defaultTitle} - ${title}` : defaultTitle}</title>
       {description && <meta name="description" content={description} />}
+      {canonical && <link rel="canonical" href={canonical} />}
     </>
   );
 }
